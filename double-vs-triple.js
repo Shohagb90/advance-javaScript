a04187abc4079bbf923c2f86c27684c8e283fe95
Shohag Miah <shohagmiah314@gmail.com>
2021-02-04 15:36:05 +0600

    object-property done

---
+++ double-vs-triple.js
@@ -0,0 +1,21 @@
+// == only value check kore
+
+const first = 2;
+const second = 3;
+if (first == second) {
+    console.log("condition is true");
+}
+ else {
+    console.log("condition is false");
+}
+
+//=== value and type check kore.
+
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+}
+ else {
+    console.log("condition is false");
+}
